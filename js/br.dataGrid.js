de6bf1021b5f373b9a9f010f34cba9e778544d99
Jager Mesh <jagermesh@gmail.com>
2013-10-03 09:48:41 -0400

    no message

--- js/br.dataGrid.js
+++ js/br.dataGrid.js
@@ -104,7 +104,6 @@
           _this.events.triggerBefore('update', data);
           var $row0 = $(row[0]);
           _this.events.trigger('update', data, $row0);
-          // $(row[0]).html(s).hide().fadeIn();
           $(row[0]).html(s);
           _this.events.triggerAfter('update', data, $row0);
           return true;
