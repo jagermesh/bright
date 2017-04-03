cc375c7c6332ccef3c7b223f88eed82c8817da02
Jager Mesh <jagermesh@gmail.com>
2017-04-03 00:39:35 -0400

    fx

--- BrReadability.php
+++ BrReadability.php
@@ -72,23 +72,6 @@ class BrReadability extends BrCustomWebParser {
                                        // , 'debug' => true,
                                        )
                                 );
-    // $response = $client->request( 'GET'
-    //                             , $url
-    //                             , array( 'headers' => array( 'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36'
-    //                                                        , 'Accept'     => 'text/html, application/xhtml+xml, application/xml, application/json, text/plain, */*'
-    //                                                        // , 'Accept'     => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
-    //                                                        // , 'Accept-Encoding'     => 'gzip, deflate, sdch'
-    //                                                        // , 'Accept-Language'     => 'en-US,en;q=0.8,nl;q=0.6,uk;q=0.4'
-    //                                                        // , 'Cache-Control' => 'no-cache'
-    //                                                        // , 'Connection'     => 'keep-alive'
-    //                                                        // , 'Referer'     => $url
-    //                                                        // , 'Pragma'     => 'no-cache'
-    //                                                        // , 'Upgrade-Insecure-Requests'     => 1
-    //                                                        )
-    //                                    , 'cookies' => $jar
-    //                                    , 'debug' => true,
-    //                                    )
-    //                             );
     if ($responseBody = (string)$response->getBody()) {
       return $this->parsePage((string)$response->getBody(), $url);
     } else {
