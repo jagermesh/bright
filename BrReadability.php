0c8595dd6471aea8ff30ef226ed78316505f1c8f
Jager Mesh <jagermesh@gmail.com>
2017-04-04 20:02:33 -0400

    fx

--- BrReadability.php
+++ BrReadability.php
@@ -10,29 +10,80 @@
 
 require_once(__DIR__ . '/BrCustomWebParser.php');
 
+if (file_exists(dirname(__DIR__) . '/3rdparty/Readability/Readability.php')) {
+  require_once(dirname(__DIR__) . '/3rdparty/Readability/Readability.php');
+}
 require_once(dirname(__DIR__) . '/vendor/autoload.php');
 
 use andreskrey\Readability\HTMLParser;
 
 class BrReadability extends BrCustomWebParser {
 
+  private $parsingMode = 'intellectual';
+
+  function __construct($parsingMode = 'intellectual') {
+
+    $this->parsingMode = $parsingMode;
+
+    parent::__construct();
+
+  }
+
   function parsePage($page, $url = null) {
 
-    $readability = new HTMLParser();
+    $parser1  = new Readability($page, 'utf-8');
+    $parser2  = new HTMLParser();
 
-    try {
-      $parsed = $readability->parse($page);
-      return new BrWebParserResult( array( 'title'    => br($parsed, 'title')
-                                         , 'image'    => br($parsed, 'image')
-                                         , 'encoding' => @$parsed['article']->encoding
-                                         , 'author'   => br($parsed, 'author')
-                                         , 'excerpt'  => br($parsed, 'excerpt')
-                                         , 'content'  => br($parsed, 'html')
-                                         , 'url'      => $url
-                                         , 'html'     => $page
-                                         ));
-    } catch (Exception $e) {
-      return $this->returnDefaultResult($page, $url);
+    if ($this->parsingMode == 'standard') {
+      try {
+        br()->log(get_class($parser2) . ': ' . $url);
+        $parsed2 = $parser2->parse($page);
+        return new BrWebParserResult( array( 'title'    => br($parsed2, 'title')
+                                           , 'image'    => br($parsed2, 'image')
+                                           , 'encoding' => @$parsed2['article']->encoding
+                                           , 'author'   => br($parsed2, 'author')
+                                           , 'excerpt'  => br($parsed2, 'excerpt')
+                                           , 'content'  => br($parsed2, 'html')
+                                           , 'url'      => $url
+                                           , 'html'     => $page
+                                           ));
+      } catch (Exception $e) {
+        return $this->returnDefaultResult($page, $url);
+      }
+    } else {
+      try {
+        br()->log(get_class($parser1) . ': ' . $url);
+        $parsed1 = $parser1->getContent();
+        if (strlen(str_replace(' ', '', br($parsed1, 'content'))) > 250) {
+          return new BrWebParserResult( array( 'title'    => br($parsed1, 'title')
+                                             , 'image'    => br($parsed1, 'lead_image_url')
+                                             , 'encoding' => 'utf-8'
+                                             , 'author'   => ''
+                                             , 'excerpt'  => ''
+                                             , 'content'  => br($parsed1, 'content')
+                                             , 'url'      => $url
+                                             , 'html'     => $page
+                                             ));
+        } else {
+          throw new BrAppException('Can not parse');
+        }
+      } catch (Exception $e) {
+        try {
+          br()->log(get_class($parser2) . ': ' . $url);
+          $parsed2 = $parser2->parse($page);
+          return new BrWebParserResult( array( 'title'    => br($parsed2, 'title')
+                                             , 'image'    => br($parsed2, 'image')
+                                             , 'encoding' => @$parsed2['article']->encoding
+                                             , 'author'   => br($parsed2, 'author')
+                                             , 'excerpt'  => br($parsed2, 'excerpt')
+                                             , 'content'  => br($parsed2, 'html')
+                                             , 'url'      => $url
+                                             , 'html'     => $page
+                                             ));
+        } catch (Exception $e) {
+          return $this->returnDefaultResult($page, $url);
+        }
+      }
     }
 
   }
