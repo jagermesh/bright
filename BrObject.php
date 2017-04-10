b2bf3b918bed30e888af642122e4626168dc84ea
Jager Mesh <jagermesh@gmail.com>
2017-04-09 23:17:37 -0400

    fx

--- BrObject.php
+++ BrObject.php
@@ -9,7 +9,7 @@
  */
 
 if (file_exists(dirname(__DIR__) . '/vendor/autoload.php')) {
-  require_once dirname(__DIR__) . '/vendor/autoload.php';
+  require_once(dirname(__DIR__) . '/vendor/autoload.php');
 }
 
 class BrObject {
