ce63a8354d4f86db2cf472e6af5dff066e4893f6
Sergiy Lavryk <jagermesh@gmail.com>
2017-07-03 08:29:41 +0800

    x

--- tools/template-dbversion/database/create-patch.php
+++ tools/template-dbversion/database/create-patch.php
@@ -4,7 +4,7 @@ require_once(dirname(__DIR__).'/bright/Bright.php');
 
 if ($patchName = @$argv[1]) {
   br()->importLib('DataBasePatch');
-  BrDataBNasePatch::generatePatchScript($patchName, __DIR__);
+  BrDataBasePatch::generatePatchScript($patchName, __DIR__);
 } else {
   br()->log('Usage: php ' . basename(__FILE__) . ' PatchName');
 }
