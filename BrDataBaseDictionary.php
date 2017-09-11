a80b3a8831c4a9f02e965b7d1462a0062c0aca24
Jager Mesh <jagermesh@gmail.com>
2017-09-11 12:08:58 -0400

    fx

--- BrDataBaseDictionary.php
+++ BrDataBaseDictionary.php
@@ -14,11 +14,25 @@ class BrDataBaseDictionary extends BrObject {
               if (!strlen($value) && $columnDesc['required']) {
                 throw new BrAppException($columnTitle . ' is required field.');
               } else
-              if (strlen($value) < $columnDesc['min_length']) {
-                throw new BrAppException($columnTitle . ' is too short. Minimum length is ' . $columnDesc['min_length'] . ' character' . ($columnDesc['max_length'] > 1 ? 's' : ''));
-              } else
-              if (strlen($value) > $columnDesc['max_length']) {
-                throw new BrAppException($columnTitle . ' is too long. Maximum length is ' . $columnDesc['max_length'] . ' character' . ($columnDesc['max_length'] > 1 ? 's' : ''));
+              if (strlen($value)) {
+                if ($columnDesc['is_integer']) {
+                  if (!is_numeric($value)) {
+                    throw new BrAppException($columnTitle . ' must be numeric');
+                  } else
+                  if ($value < $columnDesc['min_value']) {
+                    throw new BrAppException($columnTitle . ' must be greater or equal to ' . $columnDesc['min_value']);
+                  } else
+                  if ($value > $columnDesc['max_value']) {
+                    throw new BrAppException($columnTitle . ' must be less or equal to ' . $columnDesc['max_value']);
+                  }
+                } else {
+                  if (strlen($value) < $columnDesc['min_length']) {
+                    throw new BrAppException($columnTitle . ' is too short. Minimum length is ' . $columnDesc['min_length'] . ' character' . ($columnDesc['max_length'] > 1 ? 's' : ''));
+                  } else
+                  if (strlen($value) > $columnDesc['max_length']) {
+                    throw new BrAppException($columnTitle . ' is too long. Maximum length is ' . $columnDesc['max_length'] . ' character' . ($columnDesc['max_length'] > 1 ? 's' : ''));
+                  }
+                }
               }
             }
           }
@@ -30,19 +44,37 @@ class BrDataBaseDictionary extends BrObject {
 
   static function generateDictionaryScript($path) {
 
-    $sql = br()->placeholder( 'SELECT col.table_name, col.column_name, col.data_type, col.is_nullable, col.character_maximum_length, col.column_comment' . "\n"
-                            . '  FROM information_schema.columns col' . "\n"
-                            . ' WHERE col.table_schema = ?' . "\n"
-                            . '   AND col.character_maximum_length IS NOT NULL' . "\n"
-                            . '   AND col.table_name NOT LIKE "tmp%"' . "\n"
-                            . '   AND col.table_name NOT LIKE "backup%"' . "\n"
-                            . '   AND col.table_name NOT LIKE "view_%"' . "\n"
-                            . '   AND col.table_name NOT LIKE "viev_%"' . "\n"
-                            . '   AND col.table_name NOT LIKE "v_%"' . "\n"
-                            . '   AND col.table_name NOT LIKE "shared_%"' . "\n"
-                            . '   AND col.table_name NOT LIKE "audit_%"' . "\n"
-                            . '   AND col.table_name NOT LIKE "br_%"' . "\n"
-                            . ' ORDER BY col.table_name, col.column_name'
+    $sql = br()->placeholder( 'SELECT col.table_name
+                                    , col.column_name
+                                    , UPPER(col.data_type) data_type
+                                    , IF(UPPER(col.data_type) = "BIGINT" OR UPPER(col.data_type) = "INT" OR UPPER(col.data_type) = "MEDIUMINT" OR UPPER(col.data_type) = "SMALLINT" OR UPPER(col.data_type) = "TINYINT", 1, 0) is_integer
+                                    , CASE WHEN UPPER(col.data_type) = "BIGINT"    THEN IF(INSTR(col.column_type, "unsigned") > 0, 0, -9223372036854775808)
+                                           WHEN UPPER(col.data_type) = "INT"       THEN IF(INSTR(col.column_type, "unsigned") > 0, 0, -2147483648)
+                                           WHEN UPPER(col.data_type) = "MEDIUMINT" THEN IF(INSTR(col.column_type, "unsigned") > 0, 0, -8388608)
+                                           WHEN UPPER(col.data_type) = "SMALLINT"  THEN IF(INSTR(col.column_type, "unsigned") > 0, 0, -32768)
+                                           WHEN UPPER(col.data_type) = "TINYINT"   THEN IF(INSTR(col.column_type, "unsigned") > 0, 0, -128)
+                                      END min_value
+                                    , CASE WHEN UPPER(col.data_type) = "BIGINT"    THEN IF(INSTR(col.column_type, "unsigned") > 0, 18446744073709551615, 9223372036854775807)
+                                           WHEN UPPER(col.data_type) = "INT"       THEN IF(INSTR(col.column_type, "unsigned") > 0, 4294967295, 2147483647)
+                                           WHEN UPPER(col.data_type) = "MEDIUMINT" THEN IF(INSTR(col.column_type, "unsigned") > 0, 16777215, 8388607)
+                                           WHEN UPPER(col.data_type) = "SMALLINT"  THEN IF(INSTR(col.column_type, "unsigned") > 0, 65535, 32767)
+                                           WHEN UPPER(col.data_type) = "TINYINT"   THEN IF(INSTR(col.column_type, "unsigned") > 0, 255, 127)
+                                      END max_value
+                                    , IF(col.is_nullable = "NO" AND column_default IS NULL, 1, 0) required
+                                    , IF(col.is_nullable = "NO" AND column_default IS NULL, 1, 0) min_length
+                                    , IFNULL(col.character_maximum_length, 256) max_length
+                                    , col.column_comment
+                                 FROM information_schema.columns col
+                                WHERE col.table_schema = ?
+                                  AND col.table_name NOT LIKE "tmp%"
+                                  AND col.table_name NOT LIKE "backup%"
+                                  AND col.table_name NOT LIKE "view_%"
+                                  AND col.table_name NOT LIKE "viev_%"
+                                  AND col.table_name NOT LIKE "v_%"
+                                  AND col.table_name NOT LIKE "shared_%"
+                                  AND col.table_name NOT LIKE "audit_%"
+                                  AND col.table_name NOT LIKE "br_%"
+                                ORDER BY col.table_name, col.column_name'
                             , br()->config()->get('db')['name']);
 
     $columns = br()->db()->getRows($sql);
@@ -56,9 +88,12 @@ class BrDataBaseDictionary extends BrObject {
         $schema[$column['table_name']] = [];
       }
       $schema[$column['table_name']][$column['column_name']] = [ 'data_type'      => strtoupper($column['data_type'])
-                                                               , 'required'       => ($column['is_nullable'] == 'NO') ? 1 : 0
-                                                               , 'min_length'     => ($column['is_nullable'] == 'NO') ? 1 : 0
-                                                               , 'max_length'     => $column['character_maximum_length']
+                                                               , 'required'       => $column['required']
+                                                               , 'min_length'     => $column['min_length']
+                                                               , 'max_length'     => $column['max_length']
+                                                               , 'is_integer'     => $column['is_integer']
+                                                               , 'min_value'      => $column['min_value']
+                                                               , 'max_value'      => $column['max_value']
                                                                , 'column_comment' => $column['column_comment']
                                                                ];
     }
