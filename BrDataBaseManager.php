19723845f60987cec30f41787757e68ecd211ded
Jager Mesh <jagermesh@gmail.com>
2017-09-18 10:05:08 -0400

    fx

--- BrDataBaseManager.php
+++ BrDataBaseManager.php
@@ -219,7 +219,7 @@ class BrDataBaseManager {
     $sql .= 'BEGIN' . "\n";
     $sql .= '  DECLARE auditID BIGINT UNSIGNED;' . "\n";
     $sql .= '  IF @auditDisabled IS NULL THEN' . "\n";
-    $sql .= '    INSERT INTO ' . $this->auditChangeTable . ' (action_date, table_name, action_name, object_id, author_id, ip_address) VALUES (NOW(), "'. $tableName . '", "i", NEW.id, @sessionUserID, @sessionUserIP);' . "\n";
+    $sql .= '    INSERT INTO ' . $this->auditChangeTable . ' (action_date, table_name, action_name, object_id, author_id, ip_address, context) VALUES (NOW(), "'. $tableName . '", "i", NEW.id, @sessionUserID, @sessionUserIP, @sessionAuditContext);' . "\n";
     $sql .= '    SET auditID = LAST_INSERT_ID();' . "\n";
 
     for ($i = 0; $i < count($fields); $i++) {
@@ -255,7 +255,7 @@ class BrDataBaseManager {
 
     $sql .= '       ) THEN' . "\n";
 
-    $sql .= '      INSERT INTO ' . $this->auditChangeTable . ' (action_date, table_name, action_name, object_id, author_id, ip_address) VALUES (NOW(), "'. $tableName . '", "u", NEW.id, @sessionUserID, @sessionUserIP);' . "\n";
+    $sql .= '      INSERT INTO ' . $this->auditChangeTable . ' (action_date, table_name, action_name, object_id, author_id, ip_address, context) VALUES (NOW(), "'. $tableName . '", "u", NEW.id, @sessionUserID, @sessionUserIP, @sessionAuditContext);' . "\n";
     $sql .= '      SET auditID = LAST_INSERT_ID();' . "\n";
 
     for ($i = 0; $i < count($fields); $i++) {
@@ -279,7 +279,7 @@ class BrDataBaseManager {
     $sql .= 'BEGIN' . "\n";
     $sql .= '  DECLARE auditID BIGINT UNSIGNED;' . "\n";
     $sql .= '  IF @auditDisabled IS NULL THEN' . "\n";
-    $sql .= '    INSERT INTO ' . $this->auditChangeTable . ' (action_date, table_name, action_name, object_id, author_id, ip_address) VALUES (NOW(), "'. $tableName . '", "d", OLD.id, @sessionUserID, @sessionUserIP);' . "\n";
+    $sql .= '    INSERT INTO ' . $this->auditChangeTable . ' (action_date, table_name, action_name, object_id, author_id, ip_address, context) VALUES (NOW(), "'. $tableName . '", "d", OLD.id, @sessionUserID, @sessionUserIP, @sessionAuditContext);' . "\n";
     $sql .= '    SET auditID = LAST_INSERT_ID();' . "\n";
 
     for ($i = 0; $i < count($fields); $i++) {
