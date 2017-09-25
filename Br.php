bf5d2b07d4295d9d1d9b17042f7460647c715ef5
Jager Mesh <jagermesh@gmail.com>
2017-09-25 17:33:03 -0400

    fx

--- Br.php
+++ Br.php
@@ -1048,5 +1048,34 @@ class Br extends BrSingleton {
 
   }
 
-}
+  function closureDump($c) {
 
+    $str = 'function (';
+    $r = new \ReflectionFunction($c);
+    $params = array();
+    foreach($r->getParameters() as $p) {
+        $s = '';
+        if($p->isArray()) {
+            $s .= 'array ';
+        } else if($p->getClass()) {
+            $s .= $p->getClass()->name . ' ';
+        }
+        if($p->isPassedByReference()){
+            $s .= '&';
+        }
+        $s .= '$' . $p->name;
+        if($p->isOptional()) {
+            $s .= ' = ' . var_export($p->getDefaultValue(), TRUE);
+        }
+        $params []= $s;
+    }
+    $str .= implode(', ', $params);
+    $str .= '){' . PHP_EOL;
+    $lines = file($r->getFileName());
+    for($l = $r->getStartLine(); $l < $r->getEndLine(); $l++) {
+        $str .= $lines[$l];
+    }
+    return $str;
+  }
+
+}
