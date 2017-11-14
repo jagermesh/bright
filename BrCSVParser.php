ac1d654749a1381557e45c306230671b7e67c9d0
Sergiy Lavryk <jagermesh@MACBOOKPRO13JM.local>
2017-11-14 02:04:11 -0500

    fx

--- BrCSVParser.php
+++ BrCSVParser.php
@@ -114,6 +114,7 @@ class BrCSVParser extends BrObject {
           }
           // remove The character is "\xa0" (i.e. 160), which is the standard Unicode translation for &nbsp;
           $line = str_replace(chr(160), ' ', $line);
+          $line = str_replace(chr(194), '',  $line);
           $line = str_replace(chr(180), '´', $line);
           $line = mb_convert_encoding($line, 'UTF-8');
           $line = trim($line);
