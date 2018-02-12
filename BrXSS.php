5a8059944fe19d0ee09aa1aa58cb1572a8b58f74
Sergiy Lavryk <jagermesh@MACBOOKPRO13JM.local>
2018-02-12 16:45:23 -0500

    fx

--- BrXSS.php
+++ BrXSS.php
@@ -21,39 +21,41 @@ class BrXSS extends BrSingleton {
       }
     } else {
       if (br()->HTML()->isHtml($html)) {
-        try {
-          $doc = phpQuery::newDocument($html);
-          foreach(pq($doc)->find('*') as $tag) {
-            if ($attrs = pq($tag)->attr('*')) {
-              foreach ($attrs as $name => $value) {
-                if (stripos($name, 'on') === 0) {
-                  pq($tag)->removeAttr($name);
-                } else
-                if (stripos(trim($value), 'javascript:') === 0) {
-                  if (strtolower($value) != 'javascript:;') {
-                    pq($tag)->removeAttr($name);
+        $jsonArray = is_array(@json_decode($html, true));
+        if (!$jsonArray) {
+          try {
+            try {
+              $doc = phpQuery::newDocument($html);
+              foreach(pq($doc)->find('*') as $tag) {
+                if ($attrs = pq($tag)->attr('*')) {
+                  foreach ($attrs as $name => $value) {
+                    if (stripos($name, 'on') === 0) {
+                      pq($tag)->removeAttr($name);
+                    } else
+                    if (stripos(trim($value), 'javascript:') === 0) {
+                      if (strtolower($value) != 'javascript:;') {
+                        pq($tag)->removeAttr($name);
+                      }
+                    }
                   }
                 }
               }
+              foreach(pq($doc)->find('base,meta,script,object,embed') as $style) {
+                pq($style)->remove();
+              }
+              foreach(pq($doc)->find('iframe') as $tag) {
+                if (!pq($tag)->attr('src') || !preg_match('~^(http[s]?:|)//.*?(playposit[.]com|vimeo[.]com|youtu[.]be|youtube[.]com|flickr[.]com|soundcloud[.]com|edpuzzle[.]com)~i', pq($tag)->attr('src'))) {
+                  pq($tag)->remove();
+                }
+              }
+              $html = trim($doc->html());
+            } catch (Exception $e) {
             }
+          } finally {
+            phpQuery::unloadDocuments();
           }
-          foreach(pq($doc)->find('base,meta,script,object,embed') as $style) {
-            pq($style)->remove();
-          }
-          foreach(pq($doc)->find('iframe') as $tag) {
-            if (!pq($tag)->attr('src') || !preg_match('~^(http[s]?:|)//.*?(playposit[.]com|vimeo[.]com|youtu[.]be|youtube[.]com|flickr[.]com|soundcloud[.]com|edpuzzle[.]com)~i', pq($tag)->attr('src'))) {
-              pq($tag)->remove();
-            }
-          }
-          $html = $doc->html();
-          phpQuery::unloadDocuments();
-        } catch (Exception $e) {
-          phpQuery::unloadDocuments();
         }
       }
-
-      $html = trim($html);
-
     }
 
     return $html;
