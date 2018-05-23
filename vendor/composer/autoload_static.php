fa89496a4aa4c1efe784853ca53f5de8d85e0968
Sergiy Lavryk <jagermesh@gmail.com>
2018-05-23 23:21:35 +0200

    [composer]
    fx

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -20,11 +20,22 @@ class ComposerStaticInit1b17ecc4388caa2ce9de01d092ce85b7
         ),
     );
 
+    public static $prefixesPsr0 = array (
+        'M' => 
+        array (
+            'Mustache' => 
+            array (
+                0 => __DIR__ . '/..' . '/mustache/mustache/src',
+            ),
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit1b17ecc4388caa2ce9de01d092ce85b7::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit1b17ecc4388caa2ce9de01d092ce85b7::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit1b17ecc4388caa2ce9de01d092ce85b7::$prefixesPsr0;
 
         }, null, ClassLoader::class);
     }
