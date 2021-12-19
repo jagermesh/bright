4c1543ec5c072296afbc89ff49449f3aee2253bb
Sergiy Lavryk <jagermesh@gmail.com>
2021-12-18 23:15:59 -0500

    [master]
    fx

--- Br.php
+++ Br.php
@@ -8,7 +8,13 @@
  * @package Bright Core
  */
 
-require_once(dirname(dirname(__DIR__)) . '/autoload.php');
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+  require_once(__DIR__ . '/vendor/autoload.php');
+}
+
+if (file_exists(dirname(dirname(__DIR__)) . '/autoload.php')) {
+  require_once(dirname(dirname(__DIR__)) . '/autoload.php');
+}
 
 /**
  *
